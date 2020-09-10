a4017c44ccaef109bb9199c3174b39084f106c79
Makena <soniamakena02@gmail.com>
2020-09-10 23:33:40 +0300

    generated logic for CaesarTest

--- src/test/java/Caesar/CaesarTest.java
+++ src/test/java/Caesar/CaesarTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 public class CaesarTest extends TestCase {
     @Test
-    public void
+    public void encryptDecrypt_savesMessage_String(){
+        Caesar testValue= new Caesar("Hello",7);
+        assertEquals("Hello", testValue.getPlainText());
+    }
 
 }
