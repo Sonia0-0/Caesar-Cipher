2cbcd3a8e57d31d619d2d5eb9d225dc4b8ae85d0
Makena <soniamakena02@gmail.com>
2020-09-11 01:22:10 +0300

    completed encryption method

--- src/main/java/Caesar/Caesar.java
+++ src/main/java/Caesar/Caesar.java
@@ -1,40 +1,47 @@
 package Caesar;
 
+import javax.crypto.Cipher;
+
 public class Caesar {
-    public  static String encrypt(String plaintext, int shift){
-        if(shift>26){
+    public static String encrypt(String plainText, int shift){
+        if (shift>26){
             shift = shift % 26;
         }
-        else if(shift<0){
-            shift= (shift%26)+26;
-
+        else if (shift<0){
+            shift = (shift%26)+26;
         }
+
         String cipherText = "";
-        int length = plaintext.length();
-        for (int i =0; i<length; i++){
-            char ch = plaintext.charAt(i);
-            if (Character.isLetter(ch)){
-               if (Character.isLowerCase(ch)){
-                    char c =(char) (ch+shift);//casting char
-                   if (c>'z'){
-                       cipherText += (char)(ch-(26-shift));
-                   }
-               }
-               else if (Character.isUpperCase(ch)){
+        int length = plainText.length();
+        for (int i = 0; i<length; i++ ){    //for loop to shift the letters
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)){    // ensures the characters are letters
+
+                if (Character.isLowerCase(ch)){
+
+                    char c = (char)(ch + shift);
+                    if (c>'z'){
+                        cipherText += (char)(ch -(26-shift));
+                    }
+                        else{
+                            cipherText += c;
+                    }
+
+                }
+                else if (Character.isUpperCase(ch)){
+
+                }
             }
             else {
                 cipherText += ch;
             }
-            return cipherText;
         }
         return cipherText;
-                }
+    }
 
-
-
+    public static void main(String[] args){
+        String text = "Positivity";
+        String cipher = encrypt(text,7);
+        System.out.println(cipher);
+    }
 }
-
-
-
-
-
