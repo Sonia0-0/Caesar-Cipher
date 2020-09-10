d94e3e0aae233a46feeb13a7bc6c2a33d4893a3f
Makena <soniamakena02@gmail.com>
2020-09-10 23:22:25 +0300

    generated skeleton for class Caesar.java

--- src/main/java/Caesar/Caesar.java
+++ src/main/java/Caesar/Caesar.java
@@ -1,5 +1,20 @@
 package Caesar;
 
 public class Caesar {
+ public String plainText;
+ public int shift;
 
+ public   Caesar(String plainText, int shift){
+     this.plainText= plainText;
+     this.shift = shift;
+
+ }
+    public String getPlainText() {
+        return plainText;
+    }
+
+    public int getShift() {
+        return shift;
+    }
 }
+
--- src/test/java/Caesar/CaesarTest.java
+++ src/test/java/Caesar/CaesarTest.java
@@ -1,7 +1,10 @@
 package Caesar;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class CaesarTest extends TestCase {
+    @Test
+    public void
 
 }
