0d00073ed4fb04a780e707ac4cb7d886446777d0
Makena <soniamakena02@gmail.com>
2020-09-11 08:57:12 +0300

    failed CaesarTest.java

--- src/test/java/Caesar/CaesarTest.java
+++ src/test/java/Caesar/CaesarTest.java
@@ -1,13 +1,25 @@
-//package Caesar;
-//
-//import junit.framework.TestCase;
-//import org.junit.Test;
-//
-//public class CaesarTest extends TestCase {
-//    @Test
-//    public void encryptDecrypt_savesMessage_String(){
-//        Caesar testValue= new Caesar("Hello",7);
-//        assertEquals("Hello", testValue.getPlainText());
-//    }
-//
-//}
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+public class CaesarTest extends TestCase {
+// Method names have the test prefix to make the tests visible to Junit
+
+    @Test
+    public void testtheMessage_shouldBeSonia_String(){
+        Caesar caesar= new Caesar("Sonia", 2);
+        assertEquals("Sonia", caesar.getMessage());
+    }
+
+    @Test
+    public void testEncryptDecrypt_ShiftValueiSSaved_Integer() {
+        Caesar caesar = new Caesar("Sonia", 3);
+        assertEquals(3, caesar.getShift());
+    }
+    @Test
+    public void testEncryptDecrypt_objectInstantiates() {
+        Caesar caesar = new Caesar("Sonia", 3);
+        assertEquals(true, caesar instanceof Caesar);
+
+    }
+}
